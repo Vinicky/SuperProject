b582e0f9b67bff3c76eeaaae6399c6dfd0cc6f17
Vojtěch Vinický <91117434+Vinicky@users.noreply.github.com>
2023-02-08 13:35:30 +0100

    Update conn.php

--- conn.php
+++ conn.php
@@ -3,10 +3,10 @@ $username = "lm";
 $passwd = "";
 
 
-$servername = "uvdb63.active24.cz";
-$usernameDb = "crmskchccz";
+$servername = "localhost";
+$usernameDb = "root";
 $password = "";
-$dbname = "crmskchccz";
+$dbname = "crm";
 $conn = new mysqli($servername, $usernameDb, $password, $dbname);
  $conn->query("set names utf8");
 
