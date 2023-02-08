79d602bd2b70a38e06ba48410be375cd2431bf0d
Vojtěch Vinický <91117434+Vinicky@users.noreply.github.com>
2023-02-08 13:34:58 +0100

    Update dbdriver.php

--- dbdriver.php
+++ dbdriver.php
@@ -2,45 +2,23 @@
 require_once "conn.php";
 class dbdriver{
 private $cn;
-
-
-
 function __construct ($conn) {
-
 if ($conn==null) die; 
-
     $this->cn=$conn;
-    
-
 }
-
 function query ($sql) {
-
-
 $result = $this->cn -> query($sql);
-
-// Associative array
 $row = $result -> fetch_assoc();
-
-
 return $row;
 }
-
-
-
 function selectWhere ($tab,$colm,$where) {
 $List="*";
 $res=array();
-
 if (is_array($colm)) 
     $List = implode(', ', $colm);
-    else $List=$colm; 
-        
+    else $List=$colm;      
 $sql="select $List from $tab $where";
-
 $result =$this->cn->query($sql);
-
-
 $i=1;
 if ($result = $this->cn -> query($sql)) {
   while ($row = $result -> fetch_assoc()) {
@@ -49,39 +27,21 @@ if ($result = $this->cn -> query($sql)) {
      $i++;
   }
 }
-
 return $res[1];
-
 }
-
 function select ($tab,$colm) {
 return $this->selectWhere ($tab,$colm,"");
 }
-
-
 public function insertRow($arr) {
-
 if (!is_array($arr)) return -1;
-
-     
-    
     $val="null";
     foreach ($arr as $key=>$value) {
-    
         $val .=",'$value'";  	
     }
-    
-    
     $sql="INSERT INTO drinks values($val)";
     return $this->cn -> query($sql);
-    
-    
-    
     }
-    
 public  function selectQ ($sql) {
-  
-    
   $i=0;
     if ($result = $this->cn -> query($sql)) {
       while ($row = $result -> fetch_row()) {
@@ -90,17 +50,7 @@ public  function selectQ ($sql) {
          $i++;
       }
     }
-  
   return $res;
-  
   }
-
-
-}//class
-
-
-
-
-
-
+}
 ?>
